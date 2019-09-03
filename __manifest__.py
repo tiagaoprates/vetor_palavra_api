a61da42857da37c790fe191079239ccfdfaa4069
tiagaoprates <tiagoprates_911@hotmail.com>
2019-09-03 20:20:38 -0300

    1 - Estrutura do módulo

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vetor de Palavras",
+
+    'summary': """Modulo de formatação estruturada de texto.""",
+
+    'description': """
+        Formatacao estruturada de texto
+    """,
+
+    'author': "Tiago Prates",
+
+    'category': 'Other',
+    'version': '1.0',
+
+    'depends': ['base'],
+
+    'data': [
+        'view/action.xml',
+        'view/menu.xml',
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
